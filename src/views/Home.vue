63d78ae25157791b594bb63f34fa1278e83941e3
Davyd Darlan <davydgomes27@gmail.com>
2025-03-18 17:21:04 -0300

    Criação da tela inicial

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,11 @@
 <template>
     <IonPage>
-        <h1>Tela de apresentação</h1>
-        <IonButton @click="methods.createProject">Criar novo projeto</IonButton>
+        <div id="appv-home" class="wrapper">
+            <div id="appv-home" class="content">
+                <h1 class="logo">APPV</h1>
+                <button @click="methods.createProject" class="start">Analisar USF</button>
+            </div>
+        </div> 
     </IonPage>
 </template>
 
@@ -10,7 +14,6 @@
     
     import {
         IonPage,
-        IonButton,
     } from '@ionic/vue'
  
     const router = useRouter()
@@ -21,3 +24,37 @@
         }
     }
 </script>
+
+<style scoped>
+    #appv-home.wrapper {
+        position: relative;
+        display: grid;
+        justify-content: center;
+        align-items: center;
+        width: 100%;
+        height: 100%;
+        background: #e33922;
+    }
+
+    #appv-home.content {
+        display: grid;
+        justify-content: center;
+    }
+
+    #appv-home.content .logo {
+        font-family: "Livvic", serif;
+        font-size: 5em;
+        font-weight: bold;
+        color: #ffffff;
+        margin-bottom: 20px;
+    }
+
+    #appv-home.content .start {
+        font-size: 'Open Sans';
+        font-size: 1.4em;
+        padding: 10px 12px 10px 12px;
+        border-radius: 5px;
+        color: #ffffff;
+        background: #4a5d88;
+    }
+</style>
