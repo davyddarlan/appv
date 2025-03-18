099a9a2fcd5e82c897bf3cf265f88d8c8705790f
Davyd Darlan <davydgomes27@gmail.com>
2025-03-18 14:11:01 -0300

    Remoção da nevegação tab

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,6 @@
 <template>
   <ion-app>
-    <ion-tabs>
-      <ion-router-outlet />
-      <ion-tab-bar slot="bottom">
-        <ion-tab-button tab="/" href="/">
-          <ion-icon :icon="playCircle" />
-          <ion-label>Início</ion-label>
-        </ion-tab-button>
-        <ion-tab-button tab="lista-projetos" href="/lista-projetos">
-          <ion-icon :icon="radio" />
-          <ion-label>Projetos</ion-label>
-        </ion-tab-button>
-      </ion-tab-bar>
-    </ion-tabs>
+    <ion-router-outlet />
   </ion-app>
 </template>
 
@@ -20,15 +8,5 @@
   import { 
     IonApp, 
     IonRouterOutlet,
-    IonTabs,
-    IonTabBar,
-    IonTabButton,
-    IonIcon,
-    IonLabel,
   } from '@ionic/vue';
-
-  import { 
-    playCircle,
-    radio,
-  } from 'ionicons/icons'
 </script>
